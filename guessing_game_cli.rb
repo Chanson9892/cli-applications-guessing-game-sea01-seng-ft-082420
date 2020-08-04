8fd6310149fc65d901e241eb48dc5522dfc93a4c
Chandler Hanson <chan.hanson5@gmail.com>
2020-08-04 00:49:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,13 @@
+#computer generates random number between 1-6
 def computer_random_number
   random_num = rand(5) + 1
 end
 
+#
+def prompt_user(guess_number)
+  puts "guess a number between 1 and 6"
+  user_input = guess_number
+end
 
 
 def run_guessing_game
